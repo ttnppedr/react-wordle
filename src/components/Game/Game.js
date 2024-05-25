470812d234fb4b2c87ca50b856c145e31f19fc9f
ttnppedr <priest0204@gmail.com>
2024-05-25 23:55:18 +0800

    依據回答改變樣式

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -25,7 +25,7 @@ function Game() {
 
   return (
     <>
-      <GuessResults guesses={guesses} />
+      <GuessResults guesses={guesses} answer={answer} />
       <GuessInput handleSubmitGuess={handleSubmitGuess} />
     </>
   );
